7a3a88f250dcc6a004cbe9264a5af3d27a808833
zeksdev <zeljko.bajsanski@bitseverywhere.rs>
2019-02-05 23:28:39 +0100

    Login

--- MasterDance.Mobile/src/app/components/login-failed/login-failed.component.ts
+++ MasterDance.Mobile/src/app/components/login-failed/login-failed.component.ts
@@ -2,16 +2,18 @@ import {Component, Input, OnInit} from '@angular/core';
 import {ModalController, ToastController} from '@ionic/angular';
 import {HttpClient, HttpParams} from '@angular/common/http';
 import {environment} from '../../../environments/environment';
+import {AuthService} from '../../services/auth.service';
 
 @Component({
   selector: 'app-login-failed',
   templateUrl: './login-failed.component.html',
-  styleUrls: ['./login-failed.component.scss']
+  // styleUrls: ['./login-failed.component.scss']
 })
 export class LoginFailedComponent implements OnInit {
 
   @Input() uuid;
-  constructor(private modalController: ModalController, private http: HttpClient, private toasts: ToastController) { }
+  constructor(private modalController: ModalController, private http: HttpClient, private toasts: ToastController,
+              private authService: AuthService) { }
 
   ngOnInit() {
   }
@@ -28,6 +30,7 @@ export class LoginFailedComponent implements OnInit {
               toast.present();
           },
         async err => {
+              console.log(err);
             const toast = await this.toasts.create({
                 message: 'Neuspešna registracija uređaja. Pokušajte ponovo...',
                 position: 'bottom',
@@ -40,25 +43,16 @@ export class LoginFailedComponent implements OnInit {
   }
 
   login() {
-      const payload = new HttpParams()
-          .set('grant_type', 'password')
-          .set('client_id', 'mobileapp')
-          .set('username', 'uuid')
-          .set('password', this.uuid);
-      this.http.post('http://localhost:5000/connect/token', payload,
-          {headers: {'Content-Type': 'application/x-www-form-urlencoded'}}).subscribe(
-          (token) => {
-              this.modalController.dismiss();
-           },
-          async err => {
-             const toast = await this.toasts.create({
-                 message: 'Neuspešna prijava na sistem. Pokušajte ponovo...',
-                 position: 'bottom',
-                 color: 'danger',
-                 duration: 2000
-             });
-             toast.present();
-          }
-      );
+      this.authService.login().then(() => {
+          this.modalController.dismiss();
+      }).catch(async () => {
+          const toast = await this.toasts.create({
+              message: 'Neuspešna prijava na sistem. Pokušajte ponovo...',
+              position: 'bottom',
+              color: 'danger',
+              duration: 2000
+          });
+          toast.present();
+      });
   }
 }
---
+++ MasterDance.Mobile/src/app/services/auth-interceptor.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {AuthService} from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthInterceptorService implements HttpInterceptor {
+
+  constructor(private authService: AuthService) { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    // Get the auth token from the service.
+    const authToken = this.authService.accessToken;
+
+    // Clone the request and replace the original headers with
+    // cloned headers, updated with the authorization.
+    if (authToken) {
+      const authReq = req.clone({
+        headers: req.headers.set('Authorization', `Bearer ${authToken}`)
+      });
+      return next.handle(authReq);
+    }
+    return next.handle(req);
+  }
+}
